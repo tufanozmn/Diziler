2367f8e2ea19342fba3c78ce9107436630538d52
Tufan Özmen <89133078+tufanozmn@users.noreply.github.com>
2021-08-18 18:00:26 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Diziler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dizi boyutunu giriniz;");
+            int boyut = Convert.ToInt32(Console.ReadLine());
+            int[] sayilar = new int[boyut];
+
+            var r = new Random();
+
+            for (int i = 0; i <sayilar.Length; i++)
+                 sayilar[i] = r.Next(1, 10);
+
+            foreach (var s in sayilar)
+            {
+                Console.WriteLine($"{s,5} {s*s,5}");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
